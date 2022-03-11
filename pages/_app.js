6fc6e1227a62c69749c6d7a4888d4004f8629f26
Samuel Lihn <samuellihn@gmail.com>
2022-03-10 22:47:15 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/base/normalize.css'
+import '../styles/base/globals.css'
+import "../styles/prism.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
