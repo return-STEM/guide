63646caf90373ac96908d7f29f82c48dfbd5c21c
nermalneedscoffee <35051569+nermalneedscoffee@users.noreply.github.com>
2022-05-23 16:51:11 -0400

    quick

---
+++ docs/_next/static/lDSwcnR7sRaCrDLQF8LAX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[course]","\u002F[course]\u002F[lesson]\u002Fhandout","\u002F[course]\u002F[lesson]\u002Fslides","\u002F[course]\u002F[lesson]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
